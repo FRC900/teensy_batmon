5daa07efc1536011abcb89719974b08dc7ac9f53
Torchtopher <torchtopher@gmail.com>
2022-09-26 15:59:38 -0400

    add power msg type and switch to usb

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,47 @@
+// on ros side run
+// rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 _baud:=115200
+
 // Libraries
 #include <Arduino.h>
 #include <SPI.h>
 #include <SD.h>
 #include <FlexCAN_T4.h>
-#include <NativeEthernet.h>
-//#include <NativeEthernetServer.h>
-//#include <NativeEthernetClient.h>
-//#include <Ethernet.h>
-//#include <QTRSensors.h>
 #include "sfp200teensy.h"
-
 // ROS Libs
-//#include <ArduinoTcpHardware.h>
-#define ROSSERIAL_ARDUINO_TCP  // To use the TCP version of rosserial_arduino
 #include <ros.h>
 #include <std_msgs/String.h>
-
+// custom power message type
+#include "rosserial_msgs/Power.h"
 // Allocate PSRAM
 //  If PSRAM has been added, extmem_malloc() may be used to allocate this memory,
 //  started immediately after the EXTMEM variables.
 EXTMEM char bigBuffer[1000000];
 
-// Variables
+// LED
 int ledpin = 13;  // Pin on the onboard LED
-int ledState = LOW;  // ledState used to set the LED
 
 // Declare CAN interface
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> myCan;
-//static uint8_t hex[17] = "0123456789abcdef";
 
-// Ethernet MAC & IP
-byte mac[] = {0xde, 0xad, 0xbe, 0xef, 0x13, 0x37};
-IPAddress ip(10,9,0,20);
+// And the battery monitor to talk to
+const uint32_t battery_monitor = 0x0A100201;
 
 // Declare ROS Stuff
 ros::NodeHandle nh;  // Node handler
 std_msgs::String str_msg;
+rosserial_msgs::Power power_msg;
 ros::Publisher chatter("chatter", &str_msg);  // The ROS publisher
-
-// The ROSSerial server IP & port
-IPAddress server(10,9,0,8);
-const uint16_t serverPort = 11411;
-
-/*
-// QTR Sensor Stuff:
-QTRSensors qtr;
-const uint8_t sensorCount = 8;
-uint16_t sensorValues[sensorCount];
-*/
+ros::Publisher power_info("power_info", &power_msg);  // The ROS publisher
 
 void setup() {
   // Setup Serial
-  delay(5000);
   Serial.begin(9600);
 
   // WARNING: Serial communication is borked on macOS, so if you're using a mac, comment out this line
   // Otherwise it'll keep trying to setup serial infinitely and infinitely fail
-  while(!Serial) {; };
+  while(!Serial);
+
+  // Show that the Teensy is online
   Serial.println("Begin Teensy Startup Process");
 
   // Set the pinmode for the LED
@@ -75,38 +60,15 @@ void setup() {
   myCan.begin();
   myCan.setBaudRate(500000);
 
-  // Enable Ethernet:
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
-  }
-  else {
-    Ethernet.begin(mac, ip);
-    Serial.println("");
-    Serial.println("Ethernet connected");
-    Serial.println("IP address: ");
-    Serial.println(Ethernet.localIP());
-  }
-
   // Bring up ROS
-  nh.getHardware()->setConnection(server, serverPort);
   nh.initNode();
   nh.advertise(chatter);
+  nh.advertise(power_info);
+  // Start the SFP lib & tell it what device to talk to
+  set_target_device(battery_monitor);
 
   // Reset Coulomb Count
-  resetCoulombCount(0x0A100201);
-
-/*
-  //Check for and read the barcode on the battery:
-  qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){3,4,5,6,7,8,9,10}, sensorCount);
-  qtr.setEmitterPin(2);
-  delay(5);
-  qtr.read(sensorValues);//read more than once?
-  //values will be 0 to 2500, 0 is max reflectance (white), 2500 is minimum reflectance (black)
-  for(uint8_t i = 0; i < sensorCount; i++){
-    Serial.println(sensorValues[i]);
-  }
-*/
+  resetCoulombCount();
 }
 
 void loop() {
@@ -114,7 +76,7 @@ void loop() {
   RESET Counts - assume full battery
   Verify ROS Coms?
   0-Blink an LED
-  1-Read Battery Barcode
+  //1-Read Battery Barcode
   2-Read Battery Info from CAN
   3-Publish Battery Label and Coulomb Count
   4-Log to SD Card
@@ -122,41 +84,55 @@ void loop() {
   */
 
   // The Heartbeat (LED blinking)
-  if(ledState == LOW){
-    ledState = HIGH;
-    digitalWrite(ledpin, ledState);
-  }
-  else{
-    ledState = LOW;
-    digitalWrite(ledpin, ledState);
-  }
+  digitalWrite(ledpin, HIGH);
+  power_msg.partName = "hello";
+  power_msg.version = "fix me";
+  power_msg.serialString = "hello world!";
+  power_msg.voltage1 = 0.56712;
+  power_msg.voltage2 = 0.9999;
+  power_msg.voltage3 = 2.0;
+  power_msg.current = 0.56712;
+  power_msg.coulombs = 0.56712;
+  power_msg.temp = 99.29;
+
+  power_info.publish(&power_msg);
+  chatter.publish( &str_msg );
+
   
+  /*
+  // open the file.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(getPartName());
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  } else {
+    // if the file isn't open, pop up an error:
+    Serial.println("error opening datalog.txt");
+  }
+  */
+
   // All if the power information collected
-  Serial.println(getPartName(0x0A100201));
-  Serial.println(getVersion(0x0A100201));
-  Serial.println(getSerial(0x0A100201));
-  Serial.println(getVoltage(0x0A100201, 1));
-  Serial.println(getVoltage(0x0A100201, 2));
-  Serial.println(getVoltage(0x0A100201, 3));
-  Serial.println(getCurrent(0x0A100201));
-  Serial.println(getCoulombCount(0x0A100201));
-  Serial.println(getTemperature(0x0A100201));
+  
+  Serial.println(getPartName());
+  Serial.println(getVersion());
+  Serial.println(getSerial());
+  Serial.println(getVoltage(1));
+  Serial.println(getVoltage(2));
+  Serial.println(getVoltage(3));
+  Serial.println(getCurrent());
+  Serial.println(getCoulombCount());
+  Serial.println(getTemperature());
 
   // The Heartbeat
-  if(ledState == LOW){
-    ledState = HIGH;
-    digitalWrite(ledpin, ledState);
-  }
-  else{
-    ledState = LOW;
-    digitalWrite(ledpin, ledState);
-  }
+  digitalWrite(ledpin, LOW);
 
   // ROS Stuff
-  str_msg.data = "hello world!";
-  chatter.publish( &str_msg );
   nh.spinOnce();
 
-  //delay
+  // Delay, for the heartbeat
   delay(1000);
 }
